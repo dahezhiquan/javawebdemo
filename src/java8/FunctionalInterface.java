d9dd8391d5ce00a739001777b07cf8196d7bc9e8
dahezhiquan <3390205563@qq.com>
2022-08-27 16:47:28 +0800

    完善java内置的函数式接口

--- src/java8/FunctionalInterface.java
+++ src/java8/FunctionalInterface.java
@@ -1,8 +1,23 @@
 package java8;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Consumer;
+
 /**
  * java内置的函数式接口
- *
  */
 public class FunctionalInterface {
+
+    /**
+     * 体验Consumer接口
+     */
+    @Test
+    public void test1() {
+        happyTime(600, money -> System.out.println("给我钱" + money));
+    }
+
+    public void happyTime(double money, Consumer<Double> con) {
+        con.accept(money);
+    }
 }
